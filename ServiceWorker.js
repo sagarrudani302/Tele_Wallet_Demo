76e27a408d1eed0dd793d2abf6c9448bb193e54e
sagar rudani <sagarrudani111@gmail.com>
2024-10-11 20:09:58 +0530

    update service js file

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -5,29 +5,33 @@ const contentToCache = [
     "Build/Telegram Build 0.1.data",
     "Build/Telegram Build 0.1.wasm",
     "TemplateData/style.css"
-
 ];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
-    
     e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
+        const cache = await caches.open(cacheName);
+        console.log('[Service Worker] Caching all: app shell and content');
+        await cache.addAll(contentToCache);
     })());
 });
 
 self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
+    // Only cache GET requests
+    if (e.request.method === "GET") {
+        e.respondWith((async function () {
+            let response = await caches.match(e.request);
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            if (response) { return response; }
 
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
+            response = await fetch(e.request);
+            const cache = await caches.open(cacheName);
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+            cache.put(e.request, response.clone());
+            return response;
+        })());
+    } else {
+        // Let non-GET requests pass through without caching
+        e.respondWith(fetch(e.request));
+    }
 });
